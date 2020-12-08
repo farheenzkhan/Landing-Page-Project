5c50c3116da6040b5dca6ad03df013a5b599d9b1
Farheen-Khan <farheen_khan5@hotmial.co.uk>
2020-12-08 12:54:01 +0000

    Created nave bar and updated readme file

--- README.md
+++ README.md
@@ -1,4 +1,11 @@
 Landing Page
 Project completed by Farheen for the Front End Web Developer Nanodegree program.
-This is a project to practice manipulating the DOM an includes a dynamic nav bar completed in javascript.
-Each section has scroll function for when each item in the nav bar is clicked i.e it scrolls to the correct section when clicked.
+This is a project to practice manipulating the DOM.
+
+Functionality
+-Dynamically built navigation menu
+-Functionality that clearly defines active sections
+-Scroll functionality that scrolls users to the appropriate section
+
+ Documentation
+ There is no documentation at this time.
--- js/app.js
+++ js/app.js
@@ -23,12 +23,23 @@ const allsections = document.querySelectorAll('section');
 const activeclass = "your-active-class";
 const allSections = document.querySelectorAll('section');
 const unorderedList = document.getElementsByTagName("li");
-
+const navBarList = document.querySelector('#navbar__list');
+const navBarMenu = document.querySelector('.navbar__menu');
+const landingContainer = document.querySelector('.landing__container');
 /**
  * End Global Variables
  * Start Helper Functions
  *
 */
+let inViewport = function(elem) {
+    let bounding = elem.getBoundingClientRect();
+    return (
+        bounding.top >= 0 &&
+        bounding.left >= 0 &&
+        bounding.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+        bounding.right <= (window.innerWidth || document.documentElement.clientWidth)
+    );
+};
 
 
 /**
@@ -36,9 +47,7 @@ const unorderedList = document.getElementsByTagName("li");
  * Begin Main Functions
  *
 */
-
 // build the nav
-const navbarMenu = document.getElementById("navbar");
 const sections = document.querySelectorAll("section");
 function createNavbar(){
   for (let item of sections){
@@ -46,27 +55,22 @@ function createNavbar(){
     section.className = "menu__link";
     section.dataset.nav = item.id;
     section.innerText = item.dataset.nav;
-    navbarMenu.appendChild(section);
 
   };
 };
 createNavbar();
 
 
-
-
 //Adding click event listener on Nav
-        toplistitem.addEventListener('click', function() {
-            item.scrollIntoView({behavior: "smooth" })
-            toplistitem.id = "nav-"+item.id;
-        })
-       navbar.appendChild(Section 1);
-        //Add id
-        toplistitem.id = navbar;
-        toplistitem.classname = "navbar_menu"
-        //Add href
-        toplistitem.appendchild(toplistitem)
-        navbar.appendChild(navbar__menu);
+var elmnt = document.getElementById("section1");
+
+function scrollToTop() {
+elmnt.scrollIntoView(true); // Top
+}
+
+function scrollToBottom() {
+elmnt.scrollIntoView(false); // Bottom
+}
 
 //Nav list items
 
@@ -86,46 +90,141 @@ function populatenavbarlist() {
        node.appendChild(textnode);
        document.getElementById("navbar").appendChild(node);
 
+       // Scroll to section on link click
+       function scroll() {
+         const menuScroll = document.querySelectorAll('a');
+         for (let i = 0; i <= sections.length; i++) {
+           const top = sections[i].getBoundingClientRect().top + window.pageYOffset;
+           menuScroll[i].addEventListener('click', function (e) {
+             e.preventDefault();
+             window.scrollTo({
+               top,
+               behavior: 'smooth'
+             })
+           });
+         }
+       }
 
+       function isActive () {
+         const menuLinks = document.querySelectorAll('.menu__link');
+           for (let i = 0; i <= sections.length; i++) {
+             window.addEventListener('scroll', function (e) {
+               e.preventDefault();
+               if (inViewport(sections[i])) {
+                 sections[i].classList.add('active');
+                 menuLinks[i].classList.add('menu__active');
+               } else {
+                 sections[i].classList.remove('active');
+                 menuLinks[i].classList.remove('menu__active');
+               }
+             })
+             {once: true}
+         };
+       }
 
 // Add class 'active' to section when near top of viewport
-function setActive() {
-    window.addEventListener('scroll',function(event) {
-        for (let i = 1; i <= sections.length; i++) {
-            let section = document.getElementById(`section${i}`);
-            const itemId= document.getElementById(`nav-section${i}`);
+function activateNavLinks(id) {
+
+const navLinks = document.querySelectorAll(".navbar__link");
+
+	navLinks.forEach((navLink) => {
+		if (navLink.childNodes[0].id.substring(1) === id ) {
+			console.log(true);
+			navLink.childNodes[0].classList.add("demo-class");
+		} else {
+			console.log(false);
+			navLink.childNodes[0].classList.remove("demo-class");
+		}
+	})
+}
+
+function activeClassAssign() {
+
+	sections.forEach((section) =>
+		window.addEventListener("scroll", function(){
+			if (section.getBoundingClientRect().top + 200 < window.innerHeight &&
+				section.getBoundingClientRect().bottom + 200 > window.innerHeight) {
+				section.classList.add("your-active-class");
+				activateNavLinks(section.id);
+			} else {
+				section.classList.remove("your-active-class");
+			}
+		})
+	);
+}
 
 
-            if (
-              section.section1().top >= 0 &&
-              section.section1().left >= 0 &&
-              section.section1().bottom <=
-                (window.innerHeight || document.documentElement.clientHeight) &&
-              section.section1().right <=
-                (window.innerWidth || document.documentElement.clientWidth)
-            )
-            {
-            itemId.classList.add(activeclass);
-            section.classList.add(activeclass);
 
-             } else{
-            itemId.classList.remove(activeclass);
-            section.classList.remove(activeclass);
-        }
-        }
+//Add scroll to top function for button, only show button when past the page fold.
 
-    });
-};*
+window.onscroll = function() {
+	scrollFunction()
+};
 
+function scrollFunction() {
+	if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20){
+		myButton.style.display = "block";
+	} else {
+		myButton.style.display = "none";
+	}
+}
+
+function topFunction() {
+	document.body.scrollTop = 0;
+	document.documentElement.scrollTop = 0;
+}
 // Scroll to anchor ID using scrollTO event
 document.addEventListener("scroll", function() {
     setActive();
     function scrollToTop() {
       elmnt.scrollIntoView(true);
 
+
     document.addEventListener("scroll", function() {
     watchMeScroll.addEventListener('scroll', setActive);
     });
+
+    // Smooth scroll function
+    function scroll(){
+        for (let i = 1; i <= sections.length; i++){
+            let section = document.getElementById(`section${i}`);
+            const listId = document.getElementById(`secLi_${i}`);
+            listId.addEventListener('click', function(){
+                section.scrollIntoView({behavior: "smooth"})
+                element.scrollIntoView({behavior: "smooth", block: "end", inline: "nearest"});
+            });
+        }
+    };
+
+    scroll();
+
+
+
+    //scroll to top smoothly
+
+    function topFunction() {
+  	document.body.scrollTop = 0;
+  	document.documentElement.scrollTop = 0;
+  }
+
+  //Add Smooth scroll function to the anchor elements.
+
+  function smoothScroll() {
+
+  	const links = document.querySelectorAll('a');
+
+  	links.forEach((link) =>
+  		link.addEventListener('click', function(e) {
+  			e.preventDefault();
+
+  			document.querySelector(this.getAttribute("href")).scrollIntoView({
+  				behavior: 'smooth'
+  			})
+  		})
+  	);
+  }
+
+
 
 /**
  * End Main Functions
@@ -136,8 +235,51 @@ document.addEventListener("scroll", function() {
 // Build menu
 sectionList()
 // Scroll to section on link click
-
+smoothScroll();
 // Set sections as active
+function sectionActive(){
+
+    const scrollHeight = window.pageYOffset;
+    const innerHeight = window.innerHeight;
+
+
+    for(let i = 1; i <= sections.length; i++){
+
+        let section = document.getElementById(`section${i}`);
+        const listId = document.getElementById(`secLi_${i}`);
+        const secTop = section.getBoundingClientRect().top;
+        const secBot = section.getBoundingClientRect().bottom;
+        // console.log(secTop);
+        // console.log(secBot);
+        // console.log(section.offsetHeight);
+
+        // if ( (section.offsetHeight * i) <= scrollHeight <= (section.offsetHeight * (i+1))
+        // && (0 <= secTop <= section.offsetHeight)
+        // && (secBot <= section.offsetHeight)
+        // )
+        if(secTop < innerHeight/3 && secBot > innerHeight/3)
+        {
+            listId.classList.add('active');
+        }
+        else{
+            listId.classList.remove('active');
+        }
+    }
+
+    console.log(innerHeight);
+
+    //for the back to top button
+    const sec = document.getElementById('section1');
+    if (scrollHeight > sec.offsetHeight){
+        topBtn.classList.add('show-link');
+    }
+    else{
+        topBtn.classList.remove('show-link');
+    }
+
+
+
+};
 setActive()
 }
 })

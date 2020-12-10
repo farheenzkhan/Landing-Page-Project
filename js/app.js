10d0ca39384f6dbf8d0412c2dafa57a417a96cb3
Farheen-Khan <farheen_khan5@hotmial.co.uk>
2020-12-10 21:28:59 +0000

    Add scroll function

--- js/app.js
+++ js/app.js
@@ -153,6 +153,93 @@ function activeClassAssign() {
 	);
 }
 
+document.getElementById("sec1").onclick = function() {myFunction()};
+
+function myFunction() {
+  document.getElementById("sec1").innerHTML = scrollIntoView("section1");
+}
+
+
+document.body.onload = build_navbar()
+// build the navbar when the page loads
+
+function build_navbar(){
+    //loops over the sections to create the navbar with the title from every section
+    sections.forEach(build_navbar_entry)
+};
+//builds navbar by creating <li> elements for the empty navbar ul(unordered list) and adds the link according to sections.
+function build_navbar_entry(section) {
+
+    const title= section.dataset.nav; // access the dataset in sections
+    const listItem = document.createElement("li");//creates a list element
+    //const anchor =  document.createElement("a"); // creates an anchor element to be populated with a link
+    //const linkName = anchor.setAttribute('href','#'+ section.id); // sets the link according to the section so that we can scroll to section on link click
+    const linkDiv = document.createElement("div"); // creates a div that will serve as a link
+    const linkFunction = linkDiv.addEventListener('click', () =>{
+        scrollSection(section)
+    });
+    linkDiv.classList.add("navbar-link"+ "-" + section.id); // creates a class for each link (to be used in makeActive function to highlight the active item)
+    linkDiv.appendChild(document.createTextNode(title)); //append a text node with the title to the anchor
+    listItem.appendChild(linkDiv); //append anchor to the list item
+    navbar.appendChild(listItem); //append list item to navbar
+
+
+
+}
+
+//highlights the active section by adding the class called "your-active-class" for sections and the class "active" in the navbar
+function makeActive(){
+    for(const section of sections){
+     //add active class and highlight ("getBoundingClientRect()" function returns the size of an element and its position relative to the viewport)
+    const box= section.getBoundingClientRect();// where is the section in the viewport, and how big is it?
+    if(box.top <=110 && box.bottom >= 110){// if the section is between these numbers in the viewport then it means its active
+        section.classList.add("your-active-class");
+        document.querySelector(`.navbar-link-${section.id}`).classList.add("active");
+    } else {
+        //remove active class when not viewing the section, so that it removes the highlighting
+        section.classList.remove("your-active-class");
+        document.querySelector(`.navbar-link-${section.id}`).classList.remove("active");
+        }
+    }
+}
+
+
+/**
+ * End Main Functions
+ * Begin Events
+ *
+*/
+//get the button
+const button=document.querySelector(".btn");
+
+ //function that goes to the top of the document when the user presses the button
+  function goToTop(){
+      button.addEventListener('click',() => {
+      document.documentElement.scrollTop=0;
+      document.body.scrollTop=0;
+  }); }
+
+//when the user scrolls down from the top of the document, then show the button
+  function showBtn(){
+
+    if(document.body.scrollTop>100 || document.documentElement.scrollTop>100){
+        button.classList.remove('hide');
+    } else {
+        button.classList.add('hide');
+          }
+    }
+
+
+
+// Set sections as active
+// Listen to scroll to make sections active and show button
+document.addEventListener("scroll", () => {
+    makeActive();
+    showBtn();
+  }); 
+
+
+
 
 
 //Add scroll to top function for button, only show button when past the page fold.
